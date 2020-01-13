813d7f0d2366c839895f05abeb1dba01e5e9c268
Derek Miranda <derek.miranda@ff0000.com>
2020-01-13 11:41:52 -0800

    Adds ribbon toggle
    - redist

--- dist/1-build/980x250/Ad.js
+++ dist/1-build/980x250/Ad.js
@@ -1,70 +1,68 @@
-import { Core } from 'ad-control'
-import { Preflight } from '@common/js/Preflight.js'
-import { EndFrame, Main, Intro, NetflixRibbon, MainBorder } from '@common/js/Build.js'
-import { Animation } from '@common/js/Animation.js'
-import { Control } from '@common/js/Control.js'
-import { Device } from 'ad-external'
-
+import { Core } from "ad-control";
+import { Preflight } from "@common/js/Preflight.js";
+import {
+  EndFrame,
+  Main,
+  Intro,
+  NetflixRibbon,
+  MainBorder
+} from "@common/js/Build.js";
+import { Animation } from "@common/js/Animation.js";
+import { Control } from "@common/js/Control.js";
+import { Device } from "ad-external";
 
 export class Ad {
-	// called from index.html onImpression()
-	static launch(binaryAssets) {
-		console.log('Ad.launch()')
-		Core.init(binaryAssets)
-			.then(() => Preflight.init())
-			.then(() => Core.loadDynamic())
-			.then(() => Ad.prepare())
-			.catch(err => {
-				throw err
-			})
-	}
+  // called from index.html onImpression()
+  static launch(binaryAssets) {
+    console.log("Ad.launch()");
+    Core.init(binaryAssets)
+      .then(() => Preflight.init())
+      .then(() => Core.loadDynamic())
+      .then(() => Ad.prepare())
+      .catch(err => {
+        throw err;
+      });
+  }
 
-	static prepare() {
-		console.log('Ad.prepare()')
-		Control.preMarkup()
+  static prepare() {
+    console.log("Ad.prepare()");
+    Control.preMarkup();
 
+    View.main = new Main();
+    if (adData.useSupercut && Device.type === "desktop") {
+      View.intro = new Intro({ target: View.main });
+    }
 
-		View.main = new Main()
-View.endFrame = new EndFrame({
-	
-	target: View.main,
-	layout: window.Creative && Creative.layout
-})
+    View.endFrame = new EndFrame({
+      target: View.main,
+      layout: window.Creative && Creative.layout
+    });
 
-View.ribbon = new NetflixRibbon()
-View.ribbon.addEventListener('coverComplete', function(event) {
-	event.stopImmediatePropagation() // this event was coming through twice
-	Animation.playIntro()
-})
+    if (adData.useRibbon) {
+      View.ribbon = new NetflixRibbon();
+      View.ribbon.addEventListener("coverComplete", function(event) {
+        event.stopImmediatePropagation(); // this event was coming through twice
+        Animation.hideEndFrame();
+        Animation.playIntro();
+      });
 
-View.ribbon.addEventListener('leftPillarComplete', function(event) {
-	event.stopImmediatePropagation() // this event was coming through twice
-	if (!adData.useSupercut) {
-		Animation.playCreative()
-	}
-})
+      View.ribbon.addEventListener("leftPillarComplete", function(event) {
+        event.stopImmediatePropagation(); // this event was coming through twice
+        if (!adData.useSupercut) {
+          Animation.playCreative();
+        }
+      });
+    }
 
-if (adData.useSupercut && Device.type === 'desktop') {
-	View.intro = new Intro({ target: View.main })
+    View.mainBorder = new MainBorder();
+
+    if (View.monetIntegrator.hasAttribute("ready")) {
+      Control.handleMonetLoadComplete(View.monetIntegrator);
+    } else {
+      View.monetIntegrator.addEventListener("ready", function(event) {
+        Control.handleMonetLoadComplete(View.monetIntegrator);
+      });
+    }
+  }
 }
-
-View.mainBorder = new MainBorder()
-
-
-
-if (View.monetIntegrator.hasAttribute('ready')) {
-	Control.handleMonetLoadComplete(View.monetIntegrator)
-} else {
-	View.monetIntegrator.addEventListener('ready', function(event) {
-		Control.handleMonetLoadComplete(View.monetIntegrator)
-	})
-}
-
-
-
-
-	}
-
-
-}
-window.Ad = Ad
+window.Ad = Ad;
